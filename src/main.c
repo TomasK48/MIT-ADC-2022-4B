d7fc621cb1dedecd19c0a3aa60690473f0d0643b
Tomáš Konvička <tomasin.konvicka@email.cz>
2022-01-18 17:16:41 +0100

    Teplomer a nefunkci displej

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "stm8s.h"
 #include "milis.h"
 #include "spse_stm8.h"
+#include "stm8_hd44780.h"
 
 /*#include "delay.h"*/
 #include <stdio.h>*/
@@ -23,31 +24,33 @@
 void setup(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
+
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-    init_milis();
+
+    init_uart();
+    init_milis();    //spustit časovač milis
+    lcd_init();
+
     // na pinech/vstupech ADC_IN2 (PB2) a ADC_IN3 (PB3) vypneme vstupní buffer
     ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL4, DISABLE);
     ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL5, DISABLE);
-
     // při inicializaci volíme frekvenci AD převodníku mezi 1-4MHz při 3.3V
     // mezi 1-6MHz při 5V napájení
     // nastavíme clock pro ADC (16MHz / 4 = 4MHz)
     ADC2_PrescalerConfig(ADC2_PRESSEL_FCPU_D4);
-
     // volíme zarovnání výsledku (typicky vpravo, jen vyjmečně je výhodné vlevo)
     ADC2_AlignConfig(ADC2_ALIGN_RIGHT);
-    
     // nasatvíme multiplexer na některý ze vstupních kanálů
     ADC2_Select_Channel(ADC2_CHANNEL_4);
     // rozběhneme AD převodník
     ADC2_Cmd(ENABLE);
     // počkáme než se AD převodník rozběhne (~7us)
     ADC2_Startup_Wait();
-    init_uart();
 }
 
 void main(void)
 {
+    char text[32];
     uint32_t voltage = 0;
     uint32_t temperature = 0;
     uint32_t time = 0;
@@ -65,11 +68,13 @@ void main(void)
             temperature = ((uint32_t)33000 * ADCx - (uint32_t)4096000 +19968/2) / 19968;
             printf("value %ld \n \r", voltage);
             printf("teplota %ld.%ld °C \n \r", temperature/10,temperature%10);
+
+            lcd_clear();
+            lcd_gotoxy(0,1);
+            //sprintf(text,"Teplota: ",temperature/10);
+            lcd_puts("32");
         }
 
-        /*LED_FLIP; */
-        /*_delay_ms(333);*/
-        /*printf("Funguje to!!!\n");*/
     }
 }
 
